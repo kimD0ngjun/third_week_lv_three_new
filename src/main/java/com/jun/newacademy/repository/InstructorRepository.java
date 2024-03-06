40cf3cfe4c4008fbb7d21fc9bcd4bd08c541f307
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-07 07:51:14 +0900

    [feat] 강사 데이터베이스 테이블 조회 메소드 작성

---
+++ src/main/java/com/jun/newacademy/repository/InstructorRepository.java
@@ -0,0 +1,10 @@
+package com.jun.newacademy.repository;
+
+import com.jun.newacademy.entity.instructor.Instructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InstructorRepository extends JpaRepository<Instructor, Long> {
+    Optional<Instructor> findById();
+}
