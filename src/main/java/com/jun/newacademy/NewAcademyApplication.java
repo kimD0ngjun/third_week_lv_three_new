8e683a6329d28528a5efa75557808e46480e9948
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-07 06:24:17 +0900

    initial setting

---
+++ src/main/java/com/jun/newacademy/NewAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.jun.newacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewAcademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewAcademyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jun/newacademy/NewAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jun.newacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewAcademyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
