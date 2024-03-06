732bfa616b52e55fa3f4cd371a3b61e07f69c22a
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-07 07:00:33 +0900

    [feat] 회원가입 컨트롤러 작성

---
+++ src/main/java/com/jun/newacademy/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.jun.newacademy.controller;
+
+import com.jun.newacademy.dto.UserSignUpRequestDto;
+import com.jun.newacademy.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/signup")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<String> signUp(@RequestBody @Valid UserSignUpRequestDto requestDto) {
+        return userService.signUp(requestDto);
+    }
+}
