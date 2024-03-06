0c9a36986b744aecd8a706793f431bbe8a20c786
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-07 06:43:31 +0900

    [feat] 회원 관련 dto 처리

---
+++ src/main/java/com/jun/newacademy/dto/UserLoginRequestDto.java
@@ -0,0 +1,9 @@
+package com.jun.newacademy.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UserLoginRequestDto {
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/jun/newacademy/dto/UserSignUpRequestDto.java
@@ -0,0 +1,18 @@
+package com.jun.newacademy.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class UserSignUpRequestDto {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String department;
+}
