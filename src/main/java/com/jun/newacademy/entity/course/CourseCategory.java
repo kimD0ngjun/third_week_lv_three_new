4a86f8673aa2bbb5b724f6222151f66e2111cf2d
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-07 16:16:48 +0900

    [feat] 카테고리 설정에 따른 변수명 재할당

---
+++ src/main/java/com/jun/newacademy/entity/course/CourseCategory.java
@@ -0,0 +1,22 @@
+package com.jun.newacademy.entity.course;
+
+import lombok.Getter;
+
+@Getter
+public enum CourseCategory {
+    MANAGER(CourseCategory.Subject.SPRING),
+    STAFF(CourseCategory.Subject.REACT),
+    NODEJS(CourseCategory.Subject.NODEJS);
+
+    private final String category;
+
+    CourseCategory(String category) {
+        this.category = category;
+    }
+
+    public static class Subject {
+        public static final String SPRING = "SPRING";
+        public static final String REACT = "REACT";
+        public static final String NODEJS = "NODEJS";
+    }
+}
--- src/main/java/com/jun/newacademy/service/userservice/UserServiceImpl.java
+++ src/main/java/com/jun/newacademy/service/userservice/UserServiceImpl.java
@@ -27,16 +27,16 @@ public class UserServiceImpl implements UserService {
         String department = requestDto.getDepartment();
 
         // 권한 설정
-        UserAuthority role;
+        UserAuthority authority;
         if (department.equals("커리큘럼") || department.equals("개발")) {
-            role = UserAuthority.MANAGER;
+            authority = UserAuthority.MANAGER;
         } else if (department.equals("마케팅")) {
-            role = UserAuthority.STAFF;
+            authority = UserAuthority.STAFF;
         } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("유효하지 않은 부서입니다.");
         }
 
-        User user = new User(email, password, department, role);
+        User user = new User(email, password, department, authority);
         userRepository.save(user);
 
         return ResponseEntity.ok("가입에 성공하였습니다");
