f92d93337f591832331a5f1f478eef5b3e8c092b
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-07 09:23:49 +0900

    [feat] 강의 관련 전송 객체 클래스 작성

--- src/main/java/com/jun/newacademy/dto/coursedto/CourseRequestDto.java
+++ src/main/java/com/jun/newacademy/dto/coursedto/CourseRequestDto.java
@@ -1,4 +1,11 @@
 package com.jun.newacademy.dto.coursedto;
 
+import lombok.Getter;
+
+@Getter
 public class CourseRequestDto {
+    private String title;
+    private String price;
+    private String syllabus;
+    private String category;
 }
--- src/main/java/com/jun/newacademy/dto/coursedto/CourseResponseDto.java
+++ src/main/java/com/jun/newacademy/dto/coursedto/CourseResponseDto.java
@@ -1,4 +1,30 @@
 package com.jun.newacademy.dto.coursedto;
 
+import com.jun.newacademy.entity.course.Course;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class CourseResponseDto {
+    private Long id;
+    private String title;
+    private String price;
+    private String syllabus;
+    private String category;
+    private Long instructorId;
+    private String registration;
+
+    public static CourseResponseDto fromEntity(Course course) {
+        CourseResponseDto courseDTO = new CourseResponseDto();
+        courseDTO.setTitle(course.getTitle());
+        courseDTO.setPrice(course.getPrice());
+        courseDTO.setSyllabus(course.getSyllabus());
+        courseDTO.setCategory(course.getCategory());
+        courseDTO.setInstructorId(course.getInstructor().getId()); // 외래키인 instructorId 설정
+        courseDTO.setRegistration(course.getCreatedAt().toString()); // timestamped 필드인 registration 설정
+        return courseDTO;
+    }
 }
