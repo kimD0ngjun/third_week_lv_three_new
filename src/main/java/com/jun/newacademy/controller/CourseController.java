e513d9debe5901e82c4be9ec53fa270c855dcdd4
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-07 10:18:03 +0900

    [feat] 강의 관련 컨트롤러 메소드 작성

---
+++ src/main/java/com/jun/newacademy/controller/CourseController.java
@@ -0,0 +1,45 @@
+package com.jun.newacademy.controller;
+
+import com.jun.newacademy.dto.coursedto.CourseRequestDto;
+import com.jun.newacademy.dto.coursedto.CourseResponseDto;
+import com.jun.newacademy.service.courseservice.CourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/courses")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    @PostMapping
+    @Transactional
+    public CourseResponseDto save(@RequestBody CourseRequestDto requestDto) {
+        return courseService.save(requestDto);
+    }
+
+    @GetMapping("/{id}")
+    public CourseResponseDto findById(@PathVariable Long id) {
+        return courseService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public CourseResponseDto update(@PathVariable Long id, @RequestBody CourseRequestDto requestDto) {
+        return courseService.update(id, requestDto);
+    }
+
+    @GetMapping("/{category}")
+    public List<CourseResponseDto> findByCategory(@PathVariable String category) {
+        return courseService.findByCategory(category);
+    }
+
+    @GetMapping
+    public List<CourseResponseDto> findByInstructor(@RequestParam Long instructorId) {
+        return courseService.findByInstructor(instructorId);
+    }
+}
