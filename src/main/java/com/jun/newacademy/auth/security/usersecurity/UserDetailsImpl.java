41886c3499988b74c28cfc0b97f99a3c25c50199
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-08 10:03:44 +0900

    [memo] Authentication 인증 객체 내부의 3가지 요소들 중 UserDetails와 권한 저장

--- src/main/java/com/jun/newacademy/auth/security/usersecurity/UserDetailsImpl.java
+++ src/main/java/com/jun/newacademy/auth/security/usersecurity/UserDetailsImpl.java
@@ -29,14 +29,14 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        UserAuthority authorityEnum = user.getAuthority();
-        String authority = authorityEnum.getAuthority();
+        UserAuthority authorityEnum = user.getAuthority(); // 유저 권한(열거형 선언)을 UserAuthority 인스턴스에 담음
+        String authority = authorityEnum.getAuthority(); // 그 권한들을 String 값으로 가져와서 문자열 저장
 
-        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority); // 권한값을 담은 SimpleGrantedAuthority 인스턴스
         Collection<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(simpleGrantedAuthority); // 권한을 simpleGrantedAuthority로 추상화하여 관리함
+        authorities.add(simpleGrantedAuthority); // 권한을 simpleGrantedAuthority로 추상화하여 관리함(아까 말했던 권한에 따른 요청 처리를 위해서)
 
-        return authorities;
+        return authorities; // 권한'들'이 저장된(아마 요청에 따른 뽑혀지는 권한들을 저장하는 리스트일듯?) 리스트 반환
     }
 
     @Override
