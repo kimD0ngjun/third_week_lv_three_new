15926238eb5b1ebfaba8927e8b93a09fcfe6c79c
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-08 11:04:54 +0900

    [memo] Authentication 인증 객체에 담기는 Principal 기반 유저 엔티티 조회를 위한 UserDetailsService

--- src/main/java/com/jun/newacademy/auth/security/usersecurity/UserDetailsServiceImpl.java
+++ src/main/java/com/jun/newacademy/auth/security/usersecurity/UserDetailsServiceImpl.java
@@ -15,8 +15,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
+    // 아까 Authentication 인증 객체에 담을 Principal에 해당하는 UserDetails 생성
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email)
+        User user = userRepository.findByEmail(email) // 입력받은 이메일로 DB에서 유저 엔티티 조회
                 .orElseThrow(() -> new UsernameNotFoundException("Not Found " + email));
 
         return new UserDetailsImpl(user);
